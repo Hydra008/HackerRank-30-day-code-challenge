dc838dcaa7f1c7dd065b92e38ef20f3a78f62259
Karan Shah <kshah2@lakeheadu.ca>
2017-11-10 17:19:45 -0400

    Update Solution.java

--- Day 11/Solution.java
+++ Day 11/Solution.java
@@ -14,18 +14,12 @@ public class Solution {
                 arr[i][j] = in.nextInt();
             }
         }
-        for(int i=0; i < 6; i++){
-            for(int j=0; j < 6; j++){
-                System.out.print(arr[i][j]);
-            }
-            System.out.println();
-        }
         int maxValue = countNumberOfHourglass(arr);
         System.out.println(maxValue);
     }
     public static int countNumberOfHourglass (int[][] arr)
     {
-        int max = 0;
+        int max = -81;
         int numberOfHourglass=0;
         int counter =0;
         for(int i=0; i<4;i++)
